c536f6947c67ba793479ceda76905f6603345f35
Marcus Vinicius <marcus.vinicius.mvap@gmail.com>
2022-07-13 00:15:44 -0300

    Header links

--- component/Header/index.tsx
+++ component/Header/index.tsx
@@ -1,14 +1,22 @@
-import React, { ReactNode } from "react";
-import * as S from "./styled"
+import React from 'react';
+import Link from 'next/link';
+import { headerLinks } from './content';
+import * as S from './styled';
 
-type Props = {
-  children: ReactNode;
-};
-
-const Header: React.FC<Props> = (props) => (
+const Header: React.FC = () => (
   <S.Wrapper>
-    <S.Container>{props.children}</S.Container>
-  </S.Wrapper>
-)
 
-export default Header
+    <S.LinksList>
+      {headerLinks.map(({ label, href }, index) => (
+        <S.LinkContainer key={index}>
+          <Link href={href} passHref>
+            <S.Link>{label}</S.Link>
+          </Link>
+        </S.LinkContainer>
+      ))}
+    </S.LinksList>
+ 
+  </S.Wrapper>
+);
+
+export default Header;
