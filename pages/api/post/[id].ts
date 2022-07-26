f0ec83a51d610e8bc32e52c54e84d4863b0008f4
Marcus Vinicius <marcus.vinicius.mvap@gmail.com>
2022-07-26 00:53:50 -0300

    Update API posts

---
+++ pages/api/post/[id].ts
@@ -0,0 +1,96 @@
+import { Body } from '@styled-icons/boxicons-regular';
+import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '../../../lib/prisma';
+
+interface IPost {
+  title?: string;
+  content?: string;
+  preview?: string;
+  published?: boolean;
+  author: {
+    email?  : string;
+    id?: string;
+  };
+};
+
+export default async function handle(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    switch (req.method) {
+      case 'GET':
+        GET(req, res);
+        break;
+      case 'PUT':
+        PUT(req, res);
+        break;
+      case 'DELETE':
+        DELETE(req, res);
+        break;
+      default:
+        throw new Error(`O método '${req.method}' não existe`);
+    }
+  } catch (error: unknown) {
+    res.json({ ok: false, error: error.toString(), body: req.body });
+  }
+}
+
+const GET = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { id } = req.query;
+    const result = await prisma.post.findUnique({
+      where: { id: String(id) },
+      select: {
+        id: true,
+        title: true,
+        content: true,
+        published: true,
+        author: {
+          select: {
+            name: true,
+            image: true,
+          },
+        },
+      },
+    });
+    res.json({ ok: true, posts: result });
+  } catch (error) {
+    res.json({ ok: false, error: error.toString(), body: req.body });
+  }
+};
+
+const PUT = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { id } = req.query;
+    const { post } = req.body
+    let warn = ''
+    if (post.authorId || post.author) {
+      delete post.authorId
+      delete post.author
+      warn = `Não é permitido alterar o 'authorId' ou 'author{}'` 
+    }
+    const result = await prisma.post.update({
+      where: { id: String(id) },
+      data: post,
+    });
+    res.json({ ok: true, posts: result, warn });
+  } catch (error) {
+    res.json({ ok: false, error: error.toString(), body: req.body });
+  }
+};
+
+const DELETE = async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const { id } = req.query;
+    const result = await prisma.post.update({
+      where: { id: String(id) },
+      data: {
+        published: false,
+        author: {
+          connect: { email: 'trash@trash.com' }
+        }
+      }
+    });
+    res.json({ ok: true, posts: result });
+  } catch (error) {
+    res.json({ ok: false, error: error.toString(), body: req.body });
+  }
+};
